44db16dda18de77ae203dce54d7608a4c59882a9
TriniTangl <vlad.tron.27031998@gmail.com>
2019-05-15 17:35:05 +0300

    Create components
    - FloatingButtonComponent
    - GroupEditComponent
    - GroupListComponent
    - GroupComponent
    - ToDoEditComponent
    - ToDoListComponent
    - ToDoComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {AngularMaterialModule} from './angular-material/angular-material.module';
 import {ReactiveFormsModule} from '@angular/forms';
+import {GroupComponent} from './group/group.component';
+import {GroupEditComponent} from './group-edit/group-edit.component';
+import {GroupListComponent} from './group-list/group-list.component';
+import {ToDoComponent} from './to-do/to-do.component';
+import {ToDoEditComponent} from './to-do-edit/to-do-edit.component';
+import {ToDoListComponent} from './to-do-list/to-do-list.component';
+import {FloatingButtonComponent} from './floating-button/floating-button.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        GroupComponent,
+        GroupListComponent,
+        GroupEditComponent,
+        ToDoComponent,
+        ToDoEditComponent,
+        ToDoListComponent,
+        FloatingButtonComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/to-do-edit/to-do-edit.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-to-do-edit',
+    templateUrl: './to-do-edit.component.html',
+    styleUrls: ['./to-do-edit.component.scss']
+})
+export class ToDoEditComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
