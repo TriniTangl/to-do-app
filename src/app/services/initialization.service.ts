31a9a6e0e727c5e8db16d61b17671556e6e1b9a5
TriniTangl <vlad.tron.27031998@gmail.com>
2019-05-31 16:49:51 +0300

    Fixed URL variable and added redirect to /404 when will be error

--- src/app/services/initialization.service.ts
+++ src/app/services/initialization.service.ts
@@ -1,16 +1,19 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {catchError, map} from 'rxjs/operators';
+import {Router} from '@angular/router';
 import {Observable, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 import {HttpErrorResponse} from '../interfaces';
 
 @Injectable({
     providedIn: 'root'
 })
 export class InitializationService {
-    private dataLink: string = '/ass/ets/api/tasks.json';
+    private dataLink: string = '/assets/api/tasks.json';
 
-    constructor(private http: HttpClient) {
+    constructor(
+        private http: HttpClient,
+        private router: Router) {
     }
 
     public getInitialData(): Observable<any> {
@@ -19,6 +22,7 @@ export class InitializationService {
                 map(response => response),
                 catchError((error: HttpErrorResponse | any) => {
                     console.error(`Status: ${error.status}\nMessage: ${error.message}`);
+                    this.router.navigate(['/404']);
                     return throwError(error);
                 })
             );
